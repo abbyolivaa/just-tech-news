a4d31026c78f2b0b0f2c711a0c00382d155ff377
Abby Oliva <abbyoliva6@icloud.com>
2022-05-25 14:30:05 -0600

    comment section created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const res = require('express/lib/response');
 const sequelize = require('sequelize');
 const {Post, User, Comment} = require('../models');
 
-
+// get data for posts and add it to the homepage
 router.get('/', (req, res) => {
   console.log(req.session);
     Post.findAll({
@@ -40,12 +41,56 @@ router.get('/', (req, res) => {
         });
 });
 
+//return to the homepage when clicking 'login' after already logging in
 router.get('/login', (req,res) => {
   if(req.session.loggedIn) {
     res.redirect('/');
     return;
   }
   res.render('login');
+});
+
+//get single post
+router.get('/post/:id', (req, res)=> {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id', 'post_url', 'title', 'created_at', 
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model:Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model:User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User, 
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if(!dbPostData) {
+        res.status(404).json({ message: 'No post founf with this id' });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({plain: true });
+
+      // pass data to template
+      res.render('single-post', { post });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    })
 });
 
 
