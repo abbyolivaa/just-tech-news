961bb389ebd2d1ab36fceaa47edf57becf49d4d9
Abby Oliva <abbyoliva6@icloud.com>
2022-05-28 23:15:30 -0600

    error

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment, Vote } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
@@ -53,7 +54,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
@@ -66,13 +67,13 @@ router.post('/', (req, res) => {
       req.session.username = dbUserData.username;
       req.session.loggedIn = true;
 
-      req.json(dbUserData);
+      res.json(dbUserData);
     });
   })
 });
 
 // login
-router.post('/login', (req, res) => {
+router.post('/login', withAuth, (req, res) => {
   User.findOne({
     where: {
       email: req.body.email
